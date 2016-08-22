ab04b215246f7c8d42ea52ba59e2259a6f73079b
Douglas Siqueira <douglasodimar@yandex.com>
2016-08-22 15:18:18 -0400

    Primeiro Commit

---
+++ single.php
@@ -0,0 +1,217 @@
+<!doctype html>
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <title>Douglas Siqueira - Web Developer & Wordpress</title>
+
+    <!--Import Google Icon Font-->
+    <link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans|PT+Sans+Narrow" rel="stylesheet">
+    <!-- MaterializeCSS -->
+    <link rel="stylesheet" href="css/materialize.min.css">
+    <!-- Custom CSS -->
+    <link rel="stylesheet" href="css/custom.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css">
+</head>
+
+<body>
+    
+    <header class="topo">
+        <nav class="topbar">
+            <div class="nav-wrapper">
+                <div class="container-fluid">
+                    <a href="#" class="brand-logo center">
+                        <h1 class="logo">Douglas Siqueira</h1>
+                        <h2 class="slogan">Desenvolvimento, Wordpress e outras coisas...</h2>
+                    </a>
+                    <a href="#" data-activates="mobile-demo" class="button-collapse" style="display:block;"><i class="button-top material-icons">menu</i></a>
+                    <ul id="nav-mobile" class="right hide-on-med-and-down">
+                        <li><a href="http://twitter.com/#" target="_blank"><i class="fa  fa-twitter  fa-2x" style="color:#333;" title="Twitter"></i></a>
+                        </li>
+                        <li><a href="http://github.com/#" target="_blank"><i class="fa  fa-github  fa-2x " style="color:#333;" title="Github"></i></a>
+                        </li>
+                        <li><a href="https://www.linkedin.com/#" target="_blank"><i class="fa  fa-linkedin  fa-2x" style="color:#333;" title="LinkedIn"></i></a>
+                        </li>
+                    </ul>
+                    <ul class="side-nav" id="mobile-demo">
+                        <li><a href="sass.html">Sass</a>
+                        </li>
+                        <li><a href="badges.html">Components</a>
+                        </li>
+                        <li><a href="collapsible.html">Javascript</a>
+                        </li>
+                        <li><a href="mobile.html">asdasd</a>
+                        </li>
+                        <li>
+                            <div class="buscar">
+                                <input type="text" class="blue-text text-darken-2" name="search" placeholder="PESQUISAR..">
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    </header>
+
+    <article>
+        <div class="row">
+            <div class="container">
+                <h2 class="titulo-post">Como configurar um ambiente de desenvolvimento JAVA</h2>
+                <div class="postagem">
+                    <div class="imagem-destaque">
+                        <img class="responsive-img" src="http://farm6.staticflickr.com/5614/15602332537_bae1aaccd8_b.jpg">
+                    </div>
+                    
+                    <div class="info-post">
+                        <span class="col s12 m3 l3" style="padding:0;"><strong>Data:</strong> 12/02/2018</span>
+                        <span class="col s12 m9 l9" style="padding:0;"><strong>Autor:</strong> Douglas Siqueira</span>
+                        <div style="clear:both;"></div>
+                    </div>
+                    <div class="conteudo-post">
+                        <p style="text-align: justify;">Este novo avanço da tecnologia pode ser implantado no interior do olho para recuperar a visão perdida por lesões da córnea.</p>
+                        <p style="text-align: justify;">O método, que até agora tem sido demonstrado com sucesso em testes com animais, pode ter o potencial de aumentar dramaticamente o acesso a transplantes de córnea – o que poderia mudar a vida de cerca de 10 milhões de pessoas em todo o mundo.</p>
+                        <h2>Palavra do Engenheiro Biomédico</h2>
+                        <blockquote>
+                        <p style="text-align: justify;">“Acreditamos que o nosso novo tratamento funciona melhor do que uma córnea doada, e esperamos que no futuro possamos usar as células do próprio paciente, reduzindo o risco de rejeição”, diz o engenheiro biomédico Berkay Ozcelik, que liderou a pesquisa, enquanto na Universidade de Melbourne. “Mais estudos são necessários, mas esperamos ver o tratamento testado em pacientes no próximo ano.”</p>
+                        </blockquote>
+                        <h2>Córnea</h2>
+                        <p style="text-align: justify;">A córnea é a camada mais externa do olho. Para manter saudável, ela precisa ficar úmida e transparente, mas o envelhecimento, e trauma podem levar a danos na córnea, como inchaço, o que resulta na deterioração da visão.</p>
+                        <p style="text-align: justify;"><img class="aligncenter responsive-img" title="película para olhos" src="http://www.megacuriosidades.net/wp-content/uploads/2016/08/película-para-olhos.jpg" alt="recuperar a visão"></p>
+                        <p style="text-align: justify;">Transplantes de córnea são atualmente a forma mais eficaz de restaurar a visão perdida a danos na córnea, mas há uma falta significativa de córneas de doadores. Mais de 47.000 transplantes de córnea ocorreram nos EUA em 2014, mas não há o suficiente tecido do doador para satisfazer a demanda global.<br>
+                        Há também a possibilidade de rejeição de tecido de dadores de córnea, e a necessidade de tomar esteroides para lutar contra a rejeição, além de outras complicações.</p>
+                        <h2>Riscos</h2>
+                        <blockquote>
+                        <p style="text-align: justify;">“O problema com o tecido do doador é que todo o processo de manipulação, colhendo as células dos pacientes, armazenamento e depois transplantá-los pode ter efeitos prejudiciais sobre as próprias células”, Ozcelik explica à Léa Surugue no International Business Times. “Há um risco potencial para a transmissão da doença e risco de rejeição de tecidos, desde que você tenha um transplante de um corpo estranho.”</p>
+                        </blockquote>
+                        <h2>Solução</h2>
+                        <p style="text-align: justify;">A película invisível da equipe de Ozcelik desenvolveu o que poderia contornar esses problemas. A técnica, que até agora tem sido testada em ovelhas, envolve tomar uma amostra de células da córnea do sujeito, cultivando-os no filme sintético, e, em seguida, devolvê-los em maior número para o olho – onde as células regeneradas restauram as funções de umidade de bombeamento que mantem a córnea saudável e clara.</p>
+                        <blockquote>
+                        <p style="text-align: justify;">“A película de hidrogel que desenvolvemos nos permite crescer uma camada de células da córnea no laboratório”, diz Berkay num comunicado de imprensa. “Em seguida, pode-se implantar essa película sobre a superfície interna da córnea de um paciente, dentro do olho, através de uma incisão muito pequena.”</p>
+                        </blockquote>
+                        <h2>Mais fina que um Fio de cabelo</h2>
+                        <p style="text-align: justify;">A película de hidrogel é mais fina do que um fio de cabelo humano com apenas 50 micrômetros, e uma vez que as células implantadas tem restaurado a saude da água fluindo entre a córnea e interiores do olho, o filme começa a degradar-se, desaparecendo em dois meses.</p>
+                        <h2>Usos e Vantagens</h2>
+                        <blockquote>
+                        <p style="text-align: justify;">“Estes materiais mostram inflamação mínima, não causam problemas adversos em tudo e pode causar a regeneração dos tecidos, portanto, o que nos permite usar isso para várias aplicações”, disse Ozcelik Cheryl Hall, no <a href="http://www.abc.net.au/news/2016-08-15/cornea-cells-'successfully'-grown-and-implanted-cure-blindness/7736240" target="_blank">ABC</a>.</p>
+                        </blockquote>
+                        <p style="text-align: justify;">Além de remover os riscos de transmissão de doenças ou rejeição dos tecidos que acompanham o transplante de córnea convencionais, uma outra vantagem do implante da película poderia cultivar células da córnea saudáveis para uso em vários recipientes.</p>
+                        <blockquote>
+                        <p style="text-align: justify;">“A outra vantagem da nossa técnica é que, mesmo se você não usar as próprias células dos pacientes, porque podemos regenerar e aumentar o número de células de um doador em cultura, poderíamos usar o material de córnea de um doador para talvez, digamos, 20 pacientes “, disse Ozcelik International Business Times.</p>
+                        </blockquote>
+                        <h2>Testes em Coelhos</h2>
+                        <p style="text-align: justify;">Não é a primeira vez que vimos os cientistas usarem esses métodos para restaurar as funções da córnea saudáveis. No início do ano, pesquisadores do <a href="http://www.megacuriosidades.net/tags/japao/">Japão </a>e do Reino Unido descobriram que as células da córnea humana poderiam ser implantadas nos olhos de coelhos com lesões na córnea e recuperar a visão.</p>
+                        <p style="text-align: justify;">Ozcelik agora espera começar os testes clínicos com seu filme hidrogel no próximo ano, e enquanto é muito cedo para dizer se os resultados positivos em animais pode ser reproduzido com as pessoas, é incrível pensar nas possibilidades que esta técnica poderia ter.<br>
+                        Nós não podemos esperar para ver onde essa investigação pode chegar.</p>
+
+
+                        <span class="entry-post-footer">
+								<!-- TAGS DO POST -->
+								<div class="col s12 m6 l6" style="padding: 10px 0;">
+								<span class="entry-tags">
+									<span class="chip" style="margin: 2px 5px;"><a href="http://www.megacuriosidades.net/tags/biologia/" rel="tag">biologia</a></span> <span class="chip" style="margin: 2px 5px;"><a href="http://www.megacuriosidades.net/tags/lula/" rel="tag">lula</a></span> <span class="chip" style="margin: 2px 5px;"><a href="http://www.megacuriosidades.net/tags/mar/" rel="tag">mar</a></span>								</span>
+								</div>
+								<!-- BOTOES SOCIAIS DO POST -->
+								<div class="col s12 m6 l6" style="text-align: right; padding: 10px 0;">
+								<span class="entry-social">
+									<a href="http://www.facebook.com/share.php?u=http://www.megacuriosidades.net/biologos-descobrem-lula-roxa/&amp;title=Biólogos descobrem uma nova espécie de Lula Roxa no fundo do mar" onclick="window.open('http://www.facebook.com/share.php?u=http://www.megacuriosidades.net/biologos-descobrem-lula-roxa/', 'newwindow', 'width=550, height=300'); return false;"><span class="social-buttons facebook"><i class="fa fa-facebook fa-2"></i></span></a>
+
+									<a href="https://plus.google.com/share?url=http://www.megacuriosidades.net/biologos-descobrem-lula-roxa/" onclick="window.open('https://plus.google.com/share?url=http://www.megacuriosidades.net/biologos-descobrem-lula-roxa/', 'newwindow', 'width=550, height=300'); return false;"><span class="social-buttons googleplus"><i class="fa fa-google-plus fa-2"></i></span></a>
+
+									<a href="http://twitter.com/intent/tweet?status=Biólogos descobrem uma nova espécie de Lula Roxa no fundo do mar+http://www.megacuriosidades.net/biologos-descobrem-lula-roxa/" onclick="window.open('http://twitter.com/intent/tweet?status=Biólogos descobrem uma nova espécie de Lula Roxa no fundo do mar+http://www.megacuriosidades.net/biologos-descobrem-lula-roxa/', 'newwindow', 'width=550, height=300'); return false;"><span class="social-buttons twitter"><i class="fa fa-twitter fa-2"></i></span></a>
+
+									<a href="http://pinterest.com/pin/create/bookmarklet/?media=http://www.megacuriosidades.net/wp-content/uploads/2016/08/Lula-Roxa.jpg&amp;url=http://www.megacuriosidades.net/biologos-descobrem-lula-roxa/&amp;is_video=false&amp;description=Biólogos descobrem uma nova espécie de Lula Roxa no fundo do mar" onclick="window.open('http://pinterest.com/pin/create/bookmarklet/?media=http://www.megacuriosidades.net/wp-content/uploads/2016/08/Lula-Roxa.jpg&amp;url=http://www.megacuriosidades.net/biologos-descobrem-lula-roxa/&amp;is_video=false&amp;description=Biólogos descobrem uma nova espécie de Lula Roxa no fundo do mar', 'newwindow', 'width=550, height=300'); return false;"><span class="social-buttons pinterest"><i class="fa fa-pinterest fa-2"></i></span></a>
+								</span>
+								</div>
+							</span>
+                            <div style="clear:both;"></div>
+					</div>
+                </div> 
+            </div>
+        </div>
+    </article>
+
+<section>
+						<h3 class="leia-tambem center">
+							Leia Também
+						</h3>
+						<div class="container">
+							<div class="row">
+                                <div class="col s12 m6 l4">
+                                    <div class="relacionados">
+                                        <a href="#"><img src="http://emerald.sekizgen.net/wp-content/uploads/2015/07/emerald-98-1024x683.jpg" class="responsive-img"></a>
+                                        <div class="card-content">
+                                            <span class="card-title"><a href="#" class="title-post-index">Como configurar um ambiente de desenvolvimento java...</a></span>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col s12 m6 l4">
+                                    <div class="relacionados">
+                                        <a href="#"><img src="http://emerald.sekizgen.net/wp-content/uploads/2015/07/emerald-98-1024x683.jpg" class="responsive-img"></a>
+                                        <div class="card-content">
+                                            <span class="card-title"><a href="#" class="title-post-index">Como configurar um ambiente de desenvolvimento java...</a></span>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col s12 m6 l4">
+                                    <div class="relacionados">
+                                        <a href="#"><img src="http://emerald.sekizgen.net/wp-content/uploads/2015/07/emerald-98-1024x683.jpg" class="responsive-img"></a>
+                                        <div class="card-content">
+                                            <span class="card-title"><a href="#" class="title-post-index">Como configurar um ambiente de desenvolvimento java...</a></span>
+                                        </div>
+                                    </div>
+                                </div>	
+							</div>
+						</div>
+	</section>
+
+    <section>
+		<div class="container">
+            <h3 class="leia-tambem center">Deixe um Comentário</h3>
+            <div class="postagem">
+
+            </div>
+        </div>
+	</section>
+
+    <footer class="grey darken-2">
+        <div class="container">
+            <div class="row get-in-touch">
+                <div class="col l12 s12">
+                    <h5 class="white-text">ENTRE EM CONTATO</h5>
+                    <p class="grey-text text-lighten-4">
+                        Fique a vontade para entrar em contato comigo por qualquer rede social abaixo, ficarei feliz em te adicionar e responder alguma dúvida que você possa ter.
+                    </p>
+                    <div class="follow_us">
+                        <a href="#" target="_blank"><i class="fa fa-twitter"></i></a>
+                        <a href="#" target="_blank"><i class="fa fa-facebook"></i></a>
+                        <a href="#" target="_blank"><i class="fa fa-linkedin"></i></a>
+                        <a href="#" target="_blank"><i class="fa fa-google-plus"></i></a>
+                        <a href="#" target="_blank"><i class="fa fa-github"></i></a>
+						<a href="#" target="_blank"><i class="fa fa-envelope-o"></i></a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="footer-copyright grey darken-4">
+            <div class="container">
+                <p id="creditos">© 2016 SiqueiraDG, Todos os direitos reservados | Desenvolvido com <a href="https://br.wordpress.org" title="Wordpress">Wordpress</a></p>
+            </div>
+        </div>
+    </footer>
+
+    <!--Import jQuery before materialize.js-->
+    <script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+    <script type="text/javascript">
+        $(document).ready(function() {
+            $(".button-collapse").sideNav({
+                menuWidth: 240, // Default is 240
+                edge: 'left', // Choose the horizontal origin
+                closeOnClick: false // Closes side-nav on <a> clicks, useful for Angular/Meteor
+            });
+        });
+    </script>
+    <script type="text/javascript" src="js/materialize.min.js"></script>
+</body>
+
+</html>
