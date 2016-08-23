b59f3797c7a3ba1ff29010d742edb9eeba24a498
Douglas Siqueira <douglasodimar@yandex.com>
2016-08-23 11:17:27 -0400

    Atualização de erros 404

---
+++ 404.php
@@ -0,0 +1,16 @@
+<?php get_header(); ?>
+    <section>
+        <div class="container">
+            <h2 class="section-title">
+                    <span>Não Encontramos a Página</span>
+            </h2>
+            <div class="row">
+                <div class="container-fluid">
+                    <div class="postagem center">
+                        <img src="<?php bloginfo('template_directory'); ?>/images/error404.jpg" class="responsive-img">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+<?php get_footer(); ?>
